fe4c2c8371ce683203cd489eb3c8fd5a86f51323
rex <rex.s.bit@gmail.com>
2023-04-07 11:08:10 +0800

    add swap & assetsConvert examples

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,45 @@
-import { UnversalSDK } from '@defund-protocol/v1-sdk';
+import { UniversalSDK } from '@defund-protocol/v1-sdk';
 import { BigNumber, Wallet, providers } from 'ethers';
 
-const chainId = 5; // support 1 for mainnet, 5 for goerli
-const alchemyKey = ''; // rpc provider key
-const signerKey = '';  // fund gp or op private key
-const provider = new providers.AlchemyProvider(chainId, alchemyKey); // custom rpc provider
-const signer = new Wallet(signerKey, provider)
-const sdk = new UnversalSDK(chainId, signer);
+const chainId = 5;
+const provider = new providers.JsonRpcProvider('your rpc url'); // custom rpc provider
+const signer = new Wallet('your signer private key', provider)
+const sdk = new UniversalSDK(chainId, signer);
 
-const fundAddress = ''; // fund address
-const params = {
-    "opType": "exactInput", // exactInput or exactOutput
-    "tokenIn": "", // tokenIn address
-    "tokenOut": "", // tokenOut address
-    "amountIn": BigNumber.from(1), // amountIn for exactInput, amountInMaximum for exactOutput
-    "amountOut": BigNumber.from(1), // amountOutMinimum for exactInput, amountOut for exactOutput
-    "useNative": true, // if one of tokenIn or tokenOut is Weth and need use ETH balance, set true, else false
-    "expiration": 1698074828, // unix timestamp
+const fundAddress = 'your fund address';
+
+/*
+*  Swap
+*/
+const swapParams = {
+    "opType": "exactInput",
+    "tokenIn": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH Address on mainnet
+    "tokenOut": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC Address on mainnet
+    "amountIn": BigNumber.from('100000000000000000'), // 0.1 ETH
+    "amountOut": BigNumber.from('1000000'), // 1 USDC
+    "useNative": true, // use ETH
+    "expiration": Math.round(new Date().getTime() / 1000 + 10 * 60), // expires in 10 minutes
 }
 
-// options are optional
-// send {} for default, will use estimateGas.mul(110).div(100) for gasLimit
-const options = {
-    "gasPrice": 100, // max gas price in gwei,eg: 50 for 50gwei,
-    "gasLimit": 500000,
+console.log("Begin swap")
+const swapTx = await sdk.executeSwap(signer.address, fundAddress, swapParams);
+console.log("swapTx is:", swapTx)
+
+const swapResult = await swapTx.wait()
+console.log("swap result is:", swapResult)
+
+/*
+* AssetsConvert
+*/
+const convertParams = {
+    ratio: 1000, // 10% of tokenIn
+    tokenIn: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC Address on mainnet
+    tokenOut: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', // UNI Address on mainnet
 }
 
-console.log("Begin execution")
-const tx =await sdk.executeSwap(signer.address, fundAddress, params, options);
-console.log("tx is:", tx)
+console.log("Begin convert")
+const convertTx = await sdk.executeAssetsConvert(signer.address, fundAddress, convertParams);
+console.log("convert tx is:", convertTx)
 
-const result = await tx.wait()
-console.log("result is:", result)
+const convertResult = await convertTx.wait()
+console.log("convert result is:", convertResult)
